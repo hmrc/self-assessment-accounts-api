b2f9d2b8ed4174246eb0407a238986bae461f1f5
Narendra Vijayarao <26090262+narendravijayarao@users.noreply.github.com>
2020-03-02 14:00:08 +0000

    Added request parser and validator layers

---
+++ app/v1/controllers/requestParsers/ListChargesRequestDataParser.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers
+
+import javax.inject.Inject
+import uk.gov.hmrc.domain.Nino
+import v1.controllers.requestParsers.validators.ListChargesValidator
+import v1.models.request.listCharges.{ListChargesParsedRequest, ListChargesRawRequest}
+
+class ListChargesRequestDataParser @Inject()(val validator: ListChargesValidator)
+  extends RequestParser[ListChargesRawRequest, ListChargesParsedRequest] {
+
+  override protected def requestFor(data: ListChargesRawRequest): ListChargesParsedRequest =
+    ListChargesParsedRequest(Nino(data.nino), data.from.get, data.to.get)
+}
