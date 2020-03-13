1ffca003f50cd03ed0e81f028df178f701b0a965
yang <20066247+yangwong9664@users.noreply.github.com>
2020-03-13 12:02:35 +0000

    changed link factory object name

--- app/v1/models/response/retrieveTransactionDetails/RetrieveTransactionDetailsResponse.scala
+++ app/v1/models/response/retrieveTransactionDetails/RetrieveTransactionDetailsResponse.scala
@@ -30,7 +30,8 @@ object RetrieveTransactionDetailsResponse extends HateoasLinks {
 
   implicit val writes: OWrites[RetrieveTransactionDetailsResponse] = Json.writes[RetrieveTransactionDetailsResponse]
 
-  implicit object RetrieveAllocationsLinksFactory extends HateoasLinksFactory[RetrieveTransactionDetailsResponse, RetrieveTransactionDetailsHateoasData] {
+  implicit object RetrieveTransactionDetailsLinksFactory extends
+    HateoasLinksFactory[RetrieveTransactionDetailsResponse, RetrieveTransactionDetailsHateoasData] {
     override def links(appConfig: AppConfig, data: RetrieveTransactionDetailsHateoasData): Seq[Link] = {
       import data._
       Seq(
