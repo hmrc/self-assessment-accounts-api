719165406b044881eec3b5557bea851ebe077a3e
simon <55548012+simon1552@users.noreply.github.com>
2020-05-07 13:50:13 +0100

    updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -34,11 +34,11 @@ object AppDependencies {
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
     "org.scalatest"          %% "scalatest"          % "3.1.0"             % scope,
     "com.vladsch.flexmark"   % "flexmark-all"        % "0.35.10"           % scope,
-    "org.scalacheck"         %% "scalacheck"         % "1.14.2"            % scope,
+    "org.scalacheck"         %% "scalacheck"         % "1.14.3"            % scope,
     "org.scalamock"          %% "scalamock"          % "4.4.0"             % scope,
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"             % scope,
-    "com.github.tomakehurst" % "wiremock"            % "2.25.1"            % scope
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3"             % scope,
+    "com.github.tomakehurst" % "wiremock"            % "2.26.3"            % scope
   )
 
   // Fixes a transitive dependency clash between wiremock and scalatestplus-play
