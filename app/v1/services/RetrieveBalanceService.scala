cc8e6afb62bc7de0e0395ed2020825ed69c2b2cb
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2020-05-11 14:14:00 +0100

    MTDSA-5398 Updated Service Error Mappings for Retrieve a Self Assessment Balance endpoint

--- app/v1/services/RetrieveBalanceService.scala
+++ app/v1/services/RetrieveBalanceService.scala
@@ -49,8 +49,17 @@ class RetrieveBalanceService @Inject()(connector: RetrieveBalanceConnector) exte
   private def desErrorMap: Map[String, MtdError] =
     Map(
       "INVALID_IDTYPE" -> DownstreamError,
-      "INVALID_IDVALUE" -> NinoFormatError,
+      "INVALID_IDNUMBER" -> NinoFormatError,
       "INVALID_REGIME_TYPE" -> DownstreamError,
+      "INVALID_DOC_NUMBER" -> DownstreamError,
+      "INVALID_ONLY_OPEN_ITEMS" -> DownstreamError,
+      "INVALID_INCLUDE_LOCKS" -> DownstreamError,
+      "INVALID_CALCULATE_ACCRUED_INTEREST" -> DownstreamError,
+      "INVALID_CUSTOMER_PAYMENT_INFORMATION" -> DownstreamError,
+      "INVALID_DATE_FROM" -> DownstreamError,
+      "INVALID_DATE_TO" -> DownstreamError,
+      "INVALID_REMOVE_PAYMENT_ON_ACCOUNT" -> DownstreamError,
+      "REQUEST_NOT_PROCESSED" -> DownstreamError,
       "NO_DATA_FOUND" -> NotFoundError,
       "SERVER_ERROR" -> DownstreamError,
       "SERVICE_UNAVAILABLE" -> DownstreamError
