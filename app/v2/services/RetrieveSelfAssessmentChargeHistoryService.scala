3c2d7b41514b9e31e07677338e7586c861ce3636
GazalNebHMRC <108878679+GazalNebHMRC@users.noreply.github.com>
2022-08-31 20:36:53 +0100

    MTDSA-12503: Retrieve Self Assessment Charge History - Connector and Service

---
+++ app/v2/services/RetrieveSelfAssessmentChargeHistoryService.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.services
+
+import cats.data.EitherT
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.Logging
+import v2.connectors.RetrieveSelfAssessmentChargeHistoryConnector
+import v2.controllers.EndpointLogContext
+import v2.models.errors._
+import v2.models.request.retrieveSelfAssessmentChargeHistory.RetrieveSelfAssessmentChargeHistoryRequest
+import v2.models.response.retrieveSelfAssessmentChargeHistory.RetrieveSelfAssessmentChargeHistoryResponse
+import v2.support.DownstreamResponseMappingSupport
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class RetrieveSelfAssessmentChargeHistoryService @Inject()(connector: RetrieveSelfAssessmentChargeHistoryConnector)
+  extends DownstreamResponseMappingSupport
+    with Logging {
+
+  private val downstreamErrorMap =
+    Map(
+      "INVALID_CORRELATIONID" -> InternalError,
+      "INVALID_IDTYPE" -> NinoFormatError,
+      "INVALID_IDVALUE" -> InternalError,
+      "INVALID_REGIME_TYPE" -> InternalError,
+      "INVALID_DOC_NUMBER" -> TransactionIdFormatError,
+      "INVALID_DATE_FROM" -> InternalError,
+      "INVALID_DATE_TO" -> InternalError,
+      "INVALID_DATE_RANGE" -> InternalError,
+      "REQUEST_NOT_PROCESSED" -> InternalError,
+      "NO_DATA_FOUND" -> NotFoundError,
+      "SERVER_ERROR" -> InternalError,
+      "SERVICE_UNAVAILABLE" -> InternalError
+    )
+
+  def retrieve(request: RetrieveSelfAssessmentChargeHistoryRequest)(
+    implicit hc: HeaderCarrier,
+    ec: ExecutionContext,
+    logContext: EndpointLogContext,
+    correlationId: String): Future[ServiceOutcome[RetrieveSelfAssessmentChargeHistoryResponse]] = {
+
+    val result = for {
+      resultWrapper <- EitherT(connector.retrieve(request)).leftMap(mapDownstreamErrors(downstreamErrorMap))
+    } yield resultWrapper
+
+    result.value
+  }
+
+}
