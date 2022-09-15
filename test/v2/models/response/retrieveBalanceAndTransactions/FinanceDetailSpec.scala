99b90a437edb530a921e967cf12fe6aa2caf7acf
abigail-houston-hmrc <abigail.houston@digital.hmrc.gov.uk>
2022-09-15 12:36:16 +0100

    Reworded tests

--- test/v2/models/response/retrieveBalanceAndTransactions/FinanceDetailSpec.scala
+++ test/v2/models/response/retrieveBalanceAndTransactions/FinanceDetailSpec.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package v2.models.response.retrieveBalanceAndTransactions
 
 import play.api.libs.json.Json
