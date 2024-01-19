8f98520c84f1c607a3dbc776bc001d9d5ba7874d
Gaurav <41486966+GauravJai@users.noreply.github.com>
2024-01-19 12:07:58 +0000

    MTDSA-21412: updated modifier for local implicit to private

--- app/v3/models/response/retrieveCodingOutStatus/RetrieveCodingOutStatusResponse.scala
+++ app/v3/models/response/retrieveCodingOutStatus/RetrieveCodingOutStatusResponse.scala
@@ -24,7 +24,7 @@ case class RetrieveCodingOutStatusResponse(processingDate: String, nino: String,
 
 object RetrieveCodingOutStatusResponse {
 
-  implicit val downstreamIntToMtdFormat: Format[TaxYear] = Format(
+  private implicit val downstreamIntToMtdFormat: Format[TaxYear] = Format(
     implicitly[Reads[Int]].map(TaxYear.fromDownstreamInt),
     TaxYear.toMtdWrites
   )
