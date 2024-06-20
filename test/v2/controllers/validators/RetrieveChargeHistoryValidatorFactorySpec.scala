a5242435e37c1ef397dacc38ea4577d944d01296
Michael Olafisoye <molafisoye@gmail.com>
2024-06-20 09:43:17 +0100

    MTDSA-24528 amend charge history validator

--- test/v2/controllers/validators/RetrieveChargeHistoryValidatorFactorySpec.scala
+++ test/v2/controllers/validators/RetrieveChargeHistoryValidatorFactorySpec.scala
@@ -17,7 +17,7 @@
 package v2.controllers.validators
 
 import api.models.domain.{ChargeReference, Nino, TransactionId}
-import api.models.errors.{BadRequestError, ChargeReferenceFormatError, ErrorWrapper, NinoFormatError, TransactionIdFormatError}
+import api.models.errors._
 import support.UnitSpec
 import v2.models.request.retrieveChargeHistory.RetrieveChargeHistoryRequestData
 
@@ -26,8 +26,8 @@ class RetrieveChargeHistoryValidatorFactorySpec extends UnitSpec {
 
   private val validNino            = "AA123456A"
   private val validTransactionId   = "717f3agW678f"
-  private val validChargeReference      = "testCharge23"
-  private val chargeReference = ChargeReference(validChargeReference)
+  private val validChargeReference = "testCharge23"
+  private val chargeReference      = ChargeReference(validChargeReference)
 
   private val parsedNino          = Nino(validNino)
   private val parsedTransactionId = TransactionId(validTransactionId)
@@ -41,21 +41,28 @@ class RetrieveChargeHistoryValidatorFactorySpec extends UnitSpec {
     "return the parsed domain object" when {
       "passed a valid request" in {
         val result: Either[ErrorWrapper, RetrieveChargeHistoryRequestData] =
-          validator(validNino, validTransactionId, maybeChargeReference).validateAndWrapResult()
+          validator(validNino, validTransactionId, Some(validChargeReference)).validateAndWrapResult()
 
-        result shouldBe Right(RetrieveChargeHistoryRequestData(parsedNino, parsedTransactionId, validChargeReference))
+        result shouldBe Right(RetrieveChargeHistoryRequestData(parsedNino, parsedTransactionId, Some(chargeReference)))
+      }
+
+      "passed a valid request with no charge reference" in {
+        val result: Either[ErrorWrapper, RetrieveChargeHistoryRequestData] =
+          validator(validNino, validTransactionId, None).validateAndWrapResult()
+
+        result shouldBe Right(RetrieveChargeHistoryRequestData(parsedNino, parsedTransactionId, None))
       }
     }
     "should return a single error" when {
       "an invalid nino is supplied" in {
         val result: Either[ErrorWrapper, RetrieveChargeHistoryRequestData] =
-          validator("invalidNino", validTransactionId, maybeChargeReference).validateAndWrapResult()
+          validator("invalidNino", validTransactionId, Some(validChargeReference)).validateAndWrapResult()
 
         result shouldBe Left(ErrorWrapper(correlationId, NinoFormatError))
       }
       "an invalid transactionId is supplied" in {
         val result: Either[ErrorWrapper, RetrieveChargeHistoryRequestData] =
-          validator(validNino, "abcdefghijklmn", maybeChargeReference).validateAndWrapResult()
+          validator(validNino, "abcdefghijklmn", Some(validChargeReference)).validateAndWrapResult()
 
         result shouldBe Left(ErrorWrapper(correlationId, TransactionIdFormatError))
       }
