b1e3d97a61f9d8e83095c7115315fcb7ca6188a1
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2024-12-05 14:52:55 +0000

    MTDSA-26616: Adds the test for feature switch back in with a different name

--- test/config/FeatureSwitchesSpec.scala
+++ test/config/FeatureSwitchesSpec.scala
@@ -67,4 +67,38 @@ class FeatureSwitchesSpec extends UnitSpec {
 
   }
 
+  "FeatureSwitches" should {
+    "return true" when {
+      "the feature switch is set to true" in {
+        val config = Configuration(
+          "random-feature-switch.enabled" -> true
+        )
+
+        val featureSwitches = FeatureSwitches(config)
+
+        featureSwitches.isEnabled("random-feature-switch") shouldBe true
+      }
+
+      "the feature switch is not present in the config" in {
+        val config = Configuration.empty
+
+        val featureSwitches = FeatureSwitches(config)
+
+        featureSwitches.isEnabled("random-feature-switch") shouldBe true
+      }
+    }
+
+    "return false" when {
+      "the feature switch is set to false" in {
+        val config = Configuration(
+          "random-feature-switch.enabled" -> false
+        )
+
+        val featureSwitches = FeatureSwitches(config)
+
+        featureSwitches.isEnabled("random-feature-switch") shouldBe false
+      }
+    }
+  }
+
 }
