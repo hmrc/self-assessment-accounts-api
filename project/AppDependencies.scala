06edc5a18b5c77cd6d4821610819076dcc842ced
Marcy Calderon <marcycalderon@hotmail.co.uk>
2026-06-05 12:03:13 +0100

    MTDSA-34687: fix vulnerabilities by upgrading Handlebars

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,18 +23,18 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "org.typelevel"     %% "cats-core"                 % "2.13.0",
-    "com.github.jknack"  % "handlebars"                % "4.3.1"
+    "com.github.jknack"  % "handlebars"                % "4.5.1"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootstrapPlayVersion,
-    "org.scalatestplus"   %% "scalacheck-1-18"        % "3.2.19.0",
+    "org.scalatestplus"   %% "scalacheck-1-19"        % "3.2.20.0",
     "org.scalamock"       %% "scalamock"              % "7.5.5"
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = Seq(
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.21.2",
-    "io.swagger.parser.v3"         % "swagger-parser-v3"     % "2.1.39"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.22.0",
+    "io.swagger.parser.v3"         % "swagger-parser-v3"     % "2.1.43"
   ).map(_ % Test)
 
 }
