1aa120e489e1feccc8ba02b57f8867ab42d74e3e
Artemas <29547780+Artemas-Muzanenhamo@users.noreply.github.com>
2026-06-09 13:08:03 +0100

    [MTDSA-34743]: Improve logging when parsing downstream responses (#428)

--- app/shared/connectors/httpparsers/HttpParser.scala
+++ app/shared/connectors/httpparsers/HttpParser.scala
@@ -21,14 +21,33 @@ import shared.models.errors.*
 import shared.utils.Logging
 import uk.gov.hmrc.http.HttpResponse
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 trait HttpParser extends Logging {
 
   implicit class JsonResponseHelper(response: HttpResponse) {
-    private lazy val jsonOpt: Option[JsValue] = Try(response.json).toOption
+    private lazy val jsonTry: Try[JsValue] = Try(response.json)
 
-    def validateJson[T](implicit reads: Reads[T]): Option[T] = jsonOpt.flatMap(_.asOpt)
+    def validateJson[T](implicit reads: Reads[T]): Option[T] = jsonTry.toOption.flatMap(_.asOpt[T])
+
+    def validateJsonWithLogging[T](implicit reads: Reads[T]): Option[T] = {
+      jsonTry match {
+        case Success(json) =>
+          json
+            .validate[T]
+            .fold(
+              errors => {
+                logger.warn(s"[JsonResponseHelper][validateJsonWithLogging] JSON validation failed: $errors")
+                None
+              },
+              value => Some(value)
+            )
+        case Failure(_) =>
+          logger.warn("[JsonResponseHelper][validateJsonWithLogging] Response body is not valid JSON")
+          None
+      }
+    }
+
   }
 
   def retrieveCorrelationId(response: HttpResponse): String = response.header("CorrelationId").getOrElse("")
