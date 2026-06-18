b82897fb0e7b60654c7c0e479fa29f14141c5615
Johannes Bull <60427113+JoNB7777@users.noreply.github.com>
2026-06-18 09:54:03 +0100

    [MTDSA-35009] Remove RULE_REQUEST_CANNOT_BE_FULFILLED error and associated Gov-Test-Scenario (#433)

--- test/api/services/MockAuditService.scala
+++ test/api/services/MockAuditService.scala
@@ -16,11 +16,11 @@
 
 package api.services
 
+import api.models.audit.AuditEvent
 import org.scalamock.handlers.CallHandler
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.TestSuite
 import play.api.libs.json.Writes
-import api.models.audit.AuditEvent
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.audit.http.connector.AuditResult
 
